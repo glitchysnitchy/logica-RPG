e976d08e21bdc9855185d9d39109ad3839a94ca0
Carlos Vagner <47748982+glitchysnitchy@users.noreply.github.com>
2020-01-14 00:41:51 -0300

    arquivo da base de conhecimento

--- RPG-classes.pl
+++ RPG-classes.pl
@@ -1,12 +1,20 @@
-arqueiro(atento).
-barbaro(bruto, cabecadura).
-bardo(criativo).
-bruxo(esforcado).
-clerigo(metodico).
-druida(calmo).
-ladrao(silencioso, cinico).
-lutador(determinado).
-mago(sabio).
-monge(calmo).
-padre(calmo, compassivo).
-paladino(leal).
+carisma(bardo, paladino).
+constituicao(druida, monge).
+destreza(ladino, patrulheiro).
+forca(barbaro, guerreiro).
+inteligencia(feiticeiro, mago).
+sabedoria(bruxo, clerigo).
+
+armorleve(barbaro, bardo, bruxo, feiticeiro, ladino, mago, monge).
+armormedia(clerigo, druida, patrulheiro).
+armorpesada(guerreiro, paladino).
+
+hpd6(feiticeiro, mago).
+hpd8(bardo, bruxo, clerigo, druida, ladino, monge).
+hpd10(guerreiro, paladino, patrulheiro).
+hpd12(barbaro).
+
+roledps(barbaro, ladino, monge).
+rolespell(bruxo, feiticeiro, mago).
+rolesuporte(bardo, clerigo, patrulheiro).
+roletank(druida, guerreiro, paladino).
