ec9cbe79c53134cd55113f6254f2cea1292ef363
Ricardo Ariel Costa Souza <59365956+Ricardo-ACS@users.noreply.github.com>
2020-01-17 00:10:40 -0300

    Add files via upload
    ainda vou terminar de editar, o sono bateu, amanhã continuo

---
+++ RPGclasse.pl
@@ -0,0 +1,67 @@
+%fatos
+status(carisma,bardo).
+status(carisma, paladino).
+status(constituicao, druida).
+status(constituicao, monge).
+status(destreza, ladino).
+status(destreza, patrulheiro).
+status(forca, barbaro).
+status(forca, guerreiro).
+status(inteligencia, feiticeiro).
+status(inteligencia, mago).
+status(sabedoria, bruxo).
+status(sabedoria, clerigo).
+
+armadura(armorleve, barbaro).
+armadura(armorleve, bardo).
+armadura(armorleve, bruxo).
+armadura(armorleve, feiticeiro).
+armadura(armorleve, ladino).
+armadura(armorleve, mago).
+armadura(armorleve, monge).
+armadura(armormedia, clerigo).
+armadura(armormedia, druida).
+armadura(armormedia, patrulheiro).
+armadura(armorpesada, guerreiro).
+armadura(armorpesada, paladino).
+
+tipoataque(perto,barbaro).
+tipoataque(perto,monge).
+tipoataque(perto,ladino).
+tipoataque(defesa,paladino).
+tipoataque(defesa,druida).
+tipoataque(defesa,guerreiro).
+tipoataque(longe,bruxo).
+tipoataque(longe,feiticeiro).
+tipoataque(longe,mago).
+tipoataque(suporte,bardo).
+tipoataque(suporte,patrulheiro).
+tipoataque(suporte,clerigo).
+
+pontosvida(hpd6, feiticeiro).
+pontosvida(hpd6, mago).
+pontosvida(hpd8, bardo).
+pontosvida(hpd8, bruxo).
+pontosvida(hpd8, clerigo).
+pontosvida(hpd8, druida).
+pontosvida(hpd8, ladino).
+pontosvida(hpd8, monge).
+pontosvida(hpd10, guerreiro).
+pontosvida(hpd10, paladino).
+pontosvida(hpd10, patrulheiro).
+pontosvida(hpd12, barbaro).
+
+papel(dps, barbaro).
+papel(dps, ladino).
+papel(dps, monge).
+papel(spell, bruxo).
+papel(spell, feiticeiro).
+papel(spell, mago).
+papel(suporte, bardo).
+papel(suporte, clerigo).
+papel(suporte, patrulheiro).
+papel(tank, druida).
+papel(tank, guerreiro).
+papel(tank, paladino).
+
+%regras
---
+++ RPGclasse2.pl
@@ -0,0 +1,39 @@
+%fatos
+carisma(bardo).
+carisma(paladino).
+constituicao(druida).
+constituicao(monge).
+destreza(ladino).
+destreza(patrulheiro).
+forca(barbaro).
+forca(guerreiro).
+inteligencia(feiticeiro).
+inteligencia(mago).
+sabedoria(bruxo).
+sabedoria(clerigo).
+
+armorleve(bardo).
+armorleve(bruxo).
+armorleve(feiticeiro).
+armorleve(mago).
+armorleve(clerigo).
+armorleve(patrulheiro).
+armormedia(barbaro).
+armormedia(ladino).
+armormedia(monge).
+armorpesada(guerreiro).
+armorpesada(paladino).
+armorpesada(druida).
+
+pontosvida(feiticeiro, 6).
+pontosvida(mago, 6).
+pontosvida(bruxo, 8).
+pontosvida(bardo, 8).
+pontosvida(clerigo, 8).
+pontosvida(ladino, 8).
+pontosvida(druida, 8).
+pontosvida(patrulheiro, 8).
+pontosvida(monge, 10).
+pontosvida(paladino, 10).
+pontosvida(guerreiro, 12).
+pontosvida(barbaro, 12).
